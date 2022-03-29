07e30ef8101e223b0e03442d1e726cabbd6c49c2
prernapawar <100364823+prernapawar@users.noreply.github.com>
2022-03-29 18:36:57 +0530

    added arguments

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package javapractice;
+
+public class Demo {
+ public static void main(String[] args) {
+	 String[] b= 
+		 {"Prerna","Pawar"};
+	 System.out.println("Argument one =" + b[0]);
+	 System.out.println("Argument two =" + b[1]);
+	 }
+}
