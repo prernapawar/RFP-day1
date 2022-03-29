f19ddc809a3f0b9da629f8388484c5661cf216af
prernapawar <100364823+prernapawar@users.noreply.github.com>
2022-03-29 18:39:17 +0530

    added leap year

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+package javapractice;
+
+ public class LeapYear {
+ public static void main(String[] args) {
+	int year = 1582;
+	if(year % 400 == 0) {
+		System.out.println(year + "is a leap year.");
+	}else if (year % 100 == 0) {
+		System.out.println(year + "is not leap year.");
+	}else if(year % 4 == 0) {
+        System.out.println(year + "is a leap year.");
+} else {
+	System.out.println(year + "is not a leap year.");
+}
+}
+}
