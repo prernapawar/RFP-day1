31264ba49c3a59e1380380fb30a5e223937af6eb
prernapawar <100364823+prernapawar@users.noreply.github.com>
2022-03-29 18:41:57 +0530

    added two string equal or not

---
+++ compareStrings.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class compareStrings {
+ public static void main(String[] args) {
+	String style = new String("Prerna");
+	String style2 = new String("Prerna");
+	if(style.equals(style2))
+	  System.out.println("Equal");
+	else
+	   System.out.println("Not Equal");
+   }
+}
